e17f47efb2810d9f8fdbf97ba20b209f4766cefa
DavidSM14 <esilian14@gmail.com>
2024-03-11 14:17:19 +0100

    arreglo footer y correcciones en palabras

--- src/about.jsx
+++ src/about.jsx
@@ -9,9 +9,9 @@ function About() {
             <section className="py-10 bg-gray-800 lg:py-0">
                 <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
                     <div className="grid items-stretch grid-cols-1 lg:grid-cols-2 ">
-                        <div className="h-full pr-12 lg:order-2 lg:mb-40 lg:mt-20 lg:ml-10">
+                        <div className="h-full pr-12 lg:order-2 lg:mb-40 lg:mt-20 lg:ml-10 ">
                             <div className="relative h-full lg:h-auto  ">
-                                <div className="absolute w-full h-full -mb-12 overflow-hidden bg-gradient-to-r from-fuchsia-600 to-blue-600 top-12 left-12 xl:left-16 lg:top-0  lg:scale-y-100 lg:origin-top ">
+                                <div className="absolute w-full h-full mb-12 overflow-hidden bg-gradient-to-r from-fuchsia-600 to-blue-600 top-12 left-12 xl:left-16 lg:top-0  lg:scale-y-100 lg:origin-top ">
                                     <img className="object-cover object-right w-full h-full scale-150" src="https://cdn.rareblocks.xyz/collection/celebration/images/content/2/lines.svg" alt="" />
                                 </div>
                                 <div className="relative lg:-top-12 h-full ">
@@ -20,12 +20,12 @@ function About() {
                             </div>
                         </div>
 
-                        <div className="flex items-center justify-start py-10 lg:order-1 sm:py-16 md:mt-3  lg:py-24 xl:py-24">
+                        <div className="flex items-center justify-start py-10 mt-28 lg:order-1 sm:py-16 md:mt-20 sm:mt-10  lg:py-24 xl:py-24">
                             <div>
                                 <p className="text-sm font-semibold tracking-widest text-gray-500 uppercase">Dejanos ser tu guia en la busqueda de la perfección para tu hogar</p>
                                 <h2 className="mt-8 text-3xl font-bold leading-tight text-white sm:text-4xl lg:text-5xl lg:leading-tight">Descrubre la esencia del lujo italiano</h2>
-                                <p className="text-xl leading-relaxed text-gray-200 mt-9">Sumergete en el mundo de diseño de interiores de lujo con David Sanchez.
-                                    Desde magestusosas piezas de mobiliraio hasta accesorios ezquisitamente
+                                <p className="text-xl leading-relaxed text-gray-200 mt-9">Sumergete en el mundo de diseño de interiores de lujo con David Sánchez.
+                                    Desde magestusosas piezas de mobiliraio hasta accesorios exquisitamente
                                     elaborados. </p>
                                 <p className="mt-6 text-xl leading-relaxed text-gray-200">Nos dedicamos a ofrecer una experiencia de compra unica, donde cada artiuclo
                                     refleja la artesania incomparable y la pasión por el diseño que caracteriza
--- src/footer.jsx
+++ src/footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
             <div className="mx-auto sm:px-6  max-w-7xl items-center">
                 <div className="text-center xl:flex xl:items-center ">
                     <div className="xl:flex xl:items-center ">
-                        <img className="w-auto mx-auto lg:h-26 lg:translate-x-[70%] translate-x-[1/2] h-24  " src="4.png" alt="" />
+                        <img className="w-auto mx-auto lg:h-18 lg:translate-x-[150%] translate-x-[1/2] h-14  " src="4.png" alt="" />
                     </div>
                 </div>
             </div>
