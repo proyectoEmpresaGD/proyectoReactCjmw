6303d52c60c684eb55e8f5c0fe1d7c16c3369018
DavidSM14 <esilian14@gmail.com>
2024-04-18 15:50:15 +0200

    version 5.8.1

--- src/app/harbour/page.jsx
+++ src/app/harbour/page.jsx
@@ -7,7 +7,7 @@ function HarbourHome() {
         <>
             <Header/>
             <CarruselHarbour />
-            <body>
+            <body className=" bg-gradient-to-b-from">
             <div className= " flex items-center justify-center h-full">
                 <img src="/logoHarbour.png" alt="" className=" lg:w-[30%] lg:h-[20%] w-[40%] h-[30%] max-w-full max-h-full "/>
             </div>
