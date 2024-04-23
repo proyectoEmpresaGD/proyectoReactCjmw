1483f2fad93c3bfc8903f480262c0746392eb655
Gerardo Sanchez Pino <gerardosanchezpino2000@gmail.com>
2024-04-23 11:57:33 +0200

    version 5.9.1

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
                 <div className="w-full md:w-1/6 flex justify-center md:justify-start">
                     <a href="#" className="flex items-center">
                         <div className="bg-white rounded-full p-2">
-                            <img src="public\Logo.png" alt="Logo" className="w-12 h-12" />
+                            <img src="/Logo.png" alt="Logo" className="w-12 h-12" />
                         </div>
                     </a>
                 </div>
