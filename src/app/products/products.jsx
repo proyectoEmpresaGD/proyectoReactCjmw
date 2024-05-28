7bdb6a0c15e76d91d69e94fd1958d686e020c4c6
Gerardo Sanchez Pino <gerardosanchezpino2000@gmail.com>
2024-05-28 17:00:04 +0200

    version 6.2.1

--- src/app/products/products.jsx
+++ src/app/products/products.jsx
@@ -1,24 +1,25 @@
-import { Header } from "../../components/header"
-import Footer from "../../components/footer"
-import CardProduct from "../../components/ComponentesProductos/cardProduct"
-import Filtro from "../products/buttonFiltro"
+import { Header } from "../../components/header";
+import CardProduct from "../../components/ComponentesProductos/cardProduct";
+import Filtro from "../products/buttonFiltro";
 import { CartProvider } from '../../components/CartContext';
 
 function Product() {
     return (
-        <>
-        <div className=" min-h-[100%]">
+        <div className="flex flex-col min-h-screen">
             <CartProvider>
                 <Header />
-                <div className="max-w-2xl mx-auto text-center ">
-                    <h1 className="text-3xl font-bold leading-tight text-black sm:text-4xl mt-[24%] xl:mt-[18%] md:mt-[18%] lg:text-5xl">PRODUCTS</h1>
+                <div className="flex-grow">
+                    <div className="max-w-2xl mx-auto text-center mt-24 md:mt-18 xl:mt-18">
+                        <h1 className="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl">
+                            PRODUCTS
+                        </h1>
+                    </div>
+                    <Filtro />
+                    <CardProduct />
                 </div>
-                <Filtro />
-                <CardProduct />
-                <Footer />
             </CartProvider>
         </div>
-        </>
-    )
+    );
 }
-export default Product
+
+export default Product;
