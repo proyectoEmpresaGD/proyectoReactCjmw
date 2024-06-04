c8c03733daeb3b00bb049d7ed9ddfb353ed171d6
DavidSM14 <esilian14@gmail.com>
2024-06-04 09:27:04 +0200

    version 6.2.4

--- src/components/ComponentesProductos/modal.jsx
+++ src/components/ComponentesProductos/modal.jsx
@@ -6,6 +6,8 @@ import { useCart } from '../CartContext';
 import "slick-carousel/slick/slick-theme.css";
 import { TiChevronLeft, TiChevronRight } from "react-icons/ti";
 import { CartProvider } from "../CartContext"; // Importa CartProvider correctamente
+import { Link } from "react-router-dom"
+
 
 const Modal = ({ isOpen, close, product, alt }) => {
     const { addToCart } = useCart();
@@ -86,6 +88,56 @@ const Modal = ({ isOpen, close, product, alt }) => {
         setImageLoaded(false); // Reset image loaded state to show lens only after image is loaded
     };
 
+    const mantenimientoImages = {
+        "Lavable a máquina": 'https://cjmw.eu/ImagenesTelasCjmw/Iconos/washing.svg',  
+        "No lavable a máquina": "https://cjmw.eu/ImagenesTelasCjmw/Iconos/do-not-wash.svg",  
+        "Planchar a 120°": 'https://cjmw.eu/ImagenesTelasCjmw/Iconos/iron-1.svg',  
+        "Planchar a 160°": 'https://cjmw.eu/ImagenesTelasCjmw/Iconos/iron-2.svg',  
+        "Planchar a 210°": 'https://cjmw.eu/ImagenesTelasCjmw/Iconos/iron-3.svg',  
+        "No planchar": 'https://cjmw.eu/ImagenesTelasCjmw/Iconos/iron-disable.svg',  
+        "Lavar a mano": 'https://cjmw.eu/ImagenesTelasCjmw/Iconos/hand-wash.svg',  
+        "No usar blanqueador": 'https://cjmw.eu/ImagenesTelasCjmw/Iconos/noun-no-bleach.svg',  
+        "Limpieza en seco": 'https://cjmw.eu/ImagenesTelasCjmw/Iconos/dry-Clening.webp',  // Cambia esta ruta por la de tu icono
+        "No usar secadora": 'https://cjmw.eu/ImagenesTelasCjmw/Iconos/noun-do.svg',  // Cambia esta ruta por la de tu icono
+    }
+
+    const getMantenimientoImages = (mantenimientos) => {
+        if (!mantenimientos) return "?";  // Verifica si mantenimientos es null o undefined y devuelve null
+
+        const mantenimientoList = mantenimientos.split(';').map(mantenimiento => mantenimiento.trim());
+        return mantenimientoList
+            .filter(mantenimiento => mantenimientoImages[mantenimiento])  // Filtra los mantenimientos que no tienen mapeo
+            .map((mantenimiento, index) => (
+                <img
+                    key={index}
+                    src={mantenimientoImages[mantenimiento]}
+                    alt={mantenimiento}
+                    className="w-6 h-6 mx-1"
+                />
+            ));
+    };
+
+    const usoImages = {
+        "Tapiceria Decorativa": 'https://cjmw.eu/ImagenesTelasCjmw/Iconos/Upholstery%20for%20decoration.webp',
+        "Tapiceria": 'https://cjmw.eu/ImagenesTelasCjmw/Iconos/Upholstery.webp',
+        'Cortinas': 'https://cjmw.eu/ImagenesTelasCjmw/Iconos/courtains.svg',  
+        "Blinds": 'https://cjmw.eu/ImagenesTelasCjmw/Iconos/window-blind.svg',
+        "Colchas": 'https://cjmw.eu/ImagenesTelasCjmw/Iconos/bedspreads.webp',
+    };
+
+    const getUsoImages = (usos) => {
+        if (!usos) return "?";
+        const usoList = usos.split(';').map(uso => uso.trim());
+        return usoList.map((uso, index) => (
+            <img
+                key={index}
+                src={`${usoImages[uso]}`}  // Reemplaza con una imagen por defecto si no hay mapeo
+                alt={uso}
+                className="w-6 h-6 mx-1"
+            />
+        ));
+    };
+
     // Configuración del carrusel
 
     const SampleNextArrow = ({ className, style, onClick }) => {
@@ -155,86 +207,108 @@ const Modal = ({ isOpen, close, product, alt }) => {
 
     return (
         <CartProvider>
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-30 p-4">
-            <div className="bg-white p-7 lg:mt-[5%] xl:mt-[5%] md:mt-[5%] mt-[30%] rounded-lg xl:max-w-4xl w-full md:max-w-3xl m-4 h-auto overflow-auto shadow-lg relative max-h-[80vh]">
-                <div className="flex justify-end absolute top-4 right-4">
-                    <button className="relative " onClick={close}>
-                        <img src="/close.svg" className='w-8 h-8 hover:scale-125 duration-200' alt="Close" />
-                    </button>
-                </div>
-                <h2 className="text-center text-3xl font-semibold mb-4 text-gray-800 mt-12 md:mt-0">{selectedName}</h2>
-
-                <div className="grid md:grid-cols-2 grid-cols-1 gap-3" onClick={e => e.stopPropagation()}>
-                    {/* Columna del contenido */}
-                    <div className="relative group w-full h-64 md:h-96">
-                        <img
-                            src={selectedImage}
-                            alt={alt}
-                            className="w-full h-full object-contain rounded-md"
-                            onLoad={handleImageLoad}
-                            onError={handleImageError}
-                            onMouseMove={moveLens}
-                        />
-                        {imageLoaded && (
-                            <>
-                                <div
-                                    ref={lensRef}
-                                    className="absolute hidden group-hover:block w-24 h-24 border border-gray-300 opacity-50 bg-white pointer-events-none rounded-full"
-                                ></div>
-                                <div
-                                    ref={resultRef}
-                                    className="absolute hidden group-hover:block top-0 left-0 w-full h-full bg-white bg-cover pointer-events-none"
-                                    style={{
-                                        backgroundImage: `url(${selectedImage})`,
-                                        backgroundSize: `${zoomFactor * 136}%`,
-                                        borderRadius: '0.5rem'
-                                    }}
-                                >
-
-                                </div>
-                            </>
-                        )}
+            <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-30 p-4">
+                <div className="bg-white p-7 lg:mt-[5%] xl:mt-[5%] md:mt-[5%] mt-[30%] rounded-lg xl:max-w-4xl w-full md:max-w-3xl m-4 h-auto overflow-auto shadow-lg relative max-h-[90vh]">
+                    <div className="flex justify-end absolute top-4 right-4">
+                        <button className="relative " onClick={close}>
+                            <img src="/close.svg" className='w-8 h-8 hover:scale-125 duration-200' alt="Close" />
+                        </button>
                     </div>
-                    <div className="flex flex-col justify-between p-4 mx-auto">
-                        <h1 className="font-bold text-black mx-auto">Descripción</h1>
-                        <div className=" grid grid-cols-3 grid-rows-4 justify-center gap-10">
-                            <p>Marca</p>
-                            <p>Coleccion</p>
-                            <p>Color principal</p>
-                            <p className=" rounded-[100%] bg-slate-500"> </p>
-                            <p>Tipo</p>
-                            <p>Estilo</p>
-                            <p>Martindale</p>
+                    <h2 className="text-center text-3xl font-semibold mb-4 text-gray-800 mt-12 md:mt-0">{selectedName}</h2>
 
-                        </div>
-                        <div className="flex justify-between mt-4 ">
-                            <button className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full transition duration-200 mx-1"
-                                onClick={handleMapClick}>Dónde comprar</button>
-                            <button onClick={handleAddToCart} className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full transition duration-200">Adquirir muestra</button>
-                        </div>
-                    </div>
-                </div>
+                    <div className="grid md:grid-cols-2 grid-cols-1 gap-3" onClick={e => e.stopPropagation()}>
+                        {/* Columna del contenido */}
+                        <div className="relative group w-full h-64 md:h-96">
+                            <img
+                                src={selectedImage}
+                                alt={alt}
+                                className="w-full h-full object-contain rounded-md"
+                                onLoad={handleImageLoad}
+                                onError={handleImageError}
+                                onMouseMove={moveLens}
+                            />
+                            {imageLoaded && (
+                                <>
+                                    <div
+                                        ref={lensRef}
+                                        className="absolute hidden group-hover:block w-24 h-24 border border-gray-300 opacity-50 bg-white pointer-events-none rounded-full"
+                                    ></div>
+                                    <div
+                                        ref={resultRef}
+                                        className="absolute hidden group-hover:block top-0 left-0 w-full h-full bg-white bg-cover pointer-events-none"
+                                        style={{
+                                            backgroundImage: `url(${selectedImage})`,
+                                            backgroundSize: `${zoomFactor * 136}%`,
+                                            borderRadius: '0.5rem'
+                                        }}
+                                    >
 
-                {/* Carrusel de imágenes de colores */}
-                <div className="mt-6">
-                    <Slider {...settings}>
-                        {relatedProducts.map((colorProduct, index) => (
-                            <div key={index} className="px-2">
-                                <img
-                                    src={colorProduct.imagepreview}
-                                    alt={colorProduct.desprodu}
-                                    className="w-full h-16 object-cover rounded-full cursor-pointer"
-                                    onClick={() => handleColorClick(colorProduct)}
-                                />
+                                    </div>
+                                </>
+                            )}
+                        </div>
+                        <div className="flex flex-col justify-between p-2 mx-auto">
+                            <h1 className="font-bold text-black mx-auto">Descripción</h1>
+                            <div className=" grid grid-cols-3 grid-rows-4 justify-center gap-8 mx-auto text-center">
+                                <p className="rounded-full">Marca</p>
+                                <p>Coleccion</p>
+                                <p>Color principal</p>
+                                <p className="">{product.codmarca}</p>
+                                <p className="">{product.coleccion}</p>
+                                <p className="">{product.colorprincipal}</p>
+                                <p>Tipo</p>
+                                <p>Estilo</p>
+                                <p>Martindale</p>
+                                <p className="">{product.tipo}</p>
+                                <p className="">{product.estilo}</p>
+                                <p className="">{product.martindale}</p>
                             </div>
-                        ))}
-                    </Slider>
+                            <div className="grid grid-cols-2 mx-2 text-center">
+                                <div>
+                                    <h3>Usos</h3>
+                                    <Link to="/usages">
+                                        <div className="flex justify-center mt-2">
+                                            {getUsoImages(product.uso)}
+                                        </div>
+                                    </Link>
+                                </div>
+                                <div>
+                                    <h3>Mantenimiento</h3>
+                                    <Link to="/usages">
+                                        <div className="flex justify-center mt-2">
+                                            {getMantenimientoImages(product.mantenimiento)}
+                                        </div>
+                                    </Link>
+                                </div>
+                            </div>
+                            <div className="flex justify-between mt-4 ">
+                                <button className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full transition duration-200 mx-1"
+                                    onClick={handleMapClick}>Dónde comprar</button>
+                                <button onClick={handleAddToCart} className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full transition duration-200">Adquirir muestra</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* Carrusel de imágenes de colores */}
+                    <div className="mt-6">
+                        <Slider {...settings}>
+                            {relatedProducts.map((colorProduct, index) => (
+                                <div key={index} className="px-2">
+                                    <img
+                                        src={colorProduct.imagepreview}
+                                        alt={colorProduct.desprodu}
+                                        className="w-full h-16 object-cover rounded-full cursor-pointer"
+                                        onClick={() => handleColorClick(colorProduct)}
+                                    />
+                                </div>
+                            ))}
+                        </Slider>
+                    </div>
                 </div>
+                {modalMapaOpen && (
+                    <ModalMapa isOpen={modalMapaOpen} close={() => setModalMapaOpen(false)} />
+                )}
             </div>
-            {modalMapaOpen && (
-                <ModalMapa isOpen={modalMapaOpen} close={() => setModalMapaOpen(false)} />
-            )}
-        </div>
         </CartProvider>
     );
 }
