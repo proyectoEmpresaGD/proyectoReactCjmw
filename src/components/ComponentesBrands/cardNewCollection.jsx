e7559bafbdf853e9eb4e6dbe55a8d63094dd6d82
GERARDO.S <gerardosanchezpino2000@gmail.com>
2024-09-26 12:22:59 +0200

    version 6.8.5

--- src/components/ComponentesBrands/cardNewCollection.jsx
+++ src/components/ComponentesBrands/cardNewCollection.jsx
@@ -35,10 +35,6 @@ const NewCollection = ({ images, titles, productCodes }) => {
             <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
                 <header className="text-center">
                     <h2 className="text-xl font-bold text-gray-900 sm:text-3xl">New Collection</h2>
-                    <p className="mx-auto mt-4 max-w-md text-gray-500">
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque praesentium cumque iure
-                        dicta incidunt est ipsam, officia dolor fugit natus?
-                    </p>
                 </header>
 
                 <ul className="mt-8 grid grid-cols-1 gap-7 lg:grid-cols-3">
