4ee0fbc78852263cf987d4ece1e8fce5eb775098
GERARDO.S <gerardosanchezpino2000@gmail.com>
2024-10-02 18:06:41 +0200

    version 6.8.9.1

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -14,6 +14,7 @@ import 'tailwindcss/tailwind.css';
 export const Header = () => {
     const navigate = useNavigate();
     const location = useLocation();
+
     const { itemCount } = useCart();
 
     const [showCart, setShowCart] = useState(false);
