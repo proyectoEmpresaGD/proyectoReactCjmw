7d6375be3c59fb35e2652d7030f911d9b6ad324f
DavidSM14 <esilian14@gmail.com>
2025-08-29 11:17:50 +0200

    Version 6.23

--- src/components/ComponentesProductos/cardProduct.jsx
+++ src/components/ComponentesProductos/cardProduct.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef, useCallback } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import debounce from 'lodash.debounce';
-import { useCart } from '../CartContext';
+import { useCart } from '../cartContext';
 import SkeletonLoader from '../ComponentesProductos/skeletonLoader';
 import Modal from './modal';
 import Filtro from '../../app/products/buttonFiltro';
